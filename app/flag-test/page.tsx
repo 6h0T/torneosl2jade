128cc608aeeb26c73f666fad8b4bf5a354b9243d
6h0t <eliolaurencio@hotmail.com>
2025-04-16 19:50:24 -0300

    fixs

--- app/flag-test/page.tsx
+++
@@ -1,9 +0,0 @@
-import FlagTest from "@/components/flag-test"
-
-export default function FlagTestPage() {
-  return (
-    <div className="container mx-auto py-8">
-      <FlagTest />
-    </div>
-  )
-}
